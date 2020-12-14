004b154e012419ecd94dff0d6664522360cc8b9d
DharwinJRuppa <dharwinruppa1422@gmail.com>
2020-12-14 23:13:29 +0530

    geo-locator

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+var app = express();
+//set port
+var port = process.env.PORT || 8081;
+//middleware
+app.use(express.static(__dirname));
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(port, () => {
+  console.log("app running");
+});
